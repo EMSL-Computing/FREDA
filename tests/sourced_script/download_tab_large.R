3fa5101efea6b5c80c0e3d102b5ea2f44bbae7b5
Claborne, Daniel <daniel.claborne@pnnl.gov>
2018-10-08 15:47:19 -0700

    report formatting, test updates for download tab

--- tests/sourced_script/download_tab_large.R
+++ tests/sourced_script/download_tab_large.R
@@ -2,5 +2,7 @@ print("Testing the DOWNLOAD TAB, all tests through the visualize tab must pass f
 source("sourced_script/visualize_tab_large.R", local = TRUE)
 
 app$setInputs(top_page = "Download")
+
+app$setInputs(download_selection = c("separate", "merged", "group_data"))
 app$snapshotDownload("download_processed_data", filename = "images.zip")
 app$snapshot(items = list(export = "images_out"))
