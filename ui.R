700fb7f3466768a3eae0c5b3afd9a1b66b976d7e
Daniel Claborne <dmclaborne@gmail.com>
2022-06-03 10:31:21 -0700

    bump v1.0.7

--- ui.R
+++ ui.R
@@ -1,7 +1,7 @@
 
 # Define UI and wrap everything in a taglist that first calls useShinyjs()
 ui <- tagList(useShinyjs(), navbarPage(
-                   title = tags$div("FREDA", tags$span(style = "font-size:small", "v1.0.6")), 
+                   title = tags$div("FREDA", tags$span(style = "font-size:small", "v1.0.7")), 
                    windowTitle = 'FREDA',
                    id = "top_page",
                    theme = "yeti.css",
