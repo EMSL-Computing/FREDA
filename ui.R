828afd35ef6b18afdf2320ee1188d865b33f7733
Daniel Claborne <dmclaborne@gmail.com>
2024-10-16 15:21:48 -0700

    update ftmsRanalysis d4c2b9a v1.2.2

--- ui.R
+++ ui.R
@@ -15,7 +15,7 @@ ui <- tagList(useShinyjs(),
   ),
 
   navbarPage(
-    title = tags$div("FREDA", tags$span(style = "font-size:small", "v1.2.1")),
+    title = tags$div("FREDA", tags$span(style = "font-size:small", "v1.2.2")),
     windowTitle = 'FREDA',
     id = "top_page",
     theme = "yeti.css",
